dad3e614b4f635347ffecb29868d15e2e46471e7
Vyrskal <fcb.fng@gmail.com>
2022-05-31 20:08:22 +0400

    fix

--- src/asset/index.js
+++ src/asset/index.js
@@ -1,8 +1,8 @@
 let from = "AZN";
 let to = "USD";
 let rate;
-let lastCur;
 getRate = () => {
+  if (from !== to){
 fetch(`https://api.exchangerate.host/latest?base=${from}&symbols=${to}`)
 .then(res => res.json())
 .then(data => {
@@ -11,14 +11,12 @@ document.querySelector('.js-converter-rate-from').innerHTML=` 1 ${from} = ${Numb
 document.querySelector('.js-converter-rate-to').innerHTML=` 1 ${to} = ${Number((1/rate).toFixed(3))} ${from}`
 document.querySelector('.js-converter-output').value=Number((document.querySelector('.js-converter-input').value*rate).toFixed(3));
  }
-)
-return rate}
+)}}
 getRate();
 selectFrom = (cur, check) => {
     cur = cur.replace(/[^a-zA-Z]+/g, '');
-    lastCur = document.querySelector('.from.selected').innerHTML;
     if (cur == to && check === undefined){
-        selectTo(lastCur, 1);
+        selectTo(from, 1);
     }
     document.querySelector('.from.selected').classList.remove("selected");
     document.querySelector(`.from.${cur}`).classList.add("selected");
@@ -27,9 +25,8 @@ selectFrom = (cur, check) => {
 }
 selectTo = (cur, check) => {
     cur = cur.replace(/[^a-zA-Z]+/g, '');
-    lastCur = document.querySelector('.to.selected').innerHTML;
     if (cur == from && check === undefined){
-        selectFrom(lastCur, 1);
+        selectFrom(to, 1);
     }
     document.querySelector('.to.selected').classList.remove("selected");
     document.querySelector(`.to.${cur}`).classList.add("selected");
@@ -42,6 +39,22 @@ document.querySelector('.js-converter-input').addEventListener('input', () => {
 document.querySelector('.js-converter-output').addEventListener('input', () => {
     document.querySelector('.js-converter-input').value=Number((document.querySelector('.js-converter-output').value/rate).toFixed(3));
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const defaultLocale = "ru";
 let locale;
